a2ff2f8b0712a96681bf0f313d7e2a580b7b0246
Eric Darve <darve@stanford.edu>
2020-01-25 16:28:34 -0800

    Updated GPU VM script

--- Code/create_vm_gpu1.sh
+++ Code/create_vm_gpu1.sh
@@ -4,7 +4,7 @@ REGION=us-west1
 ZONE=us-west1-b
 GPU_TYPE=nvidia-tesla-k80
 GPU_NUM=1
-NAME=hw3
+NAME=gpu1
 
 # Config zone.
 gcloud config set compute/zone ${ZONE}
@@ -13,8 +13,7 @@ gcloud config set compute/zone ${ZONE}
 gcloud compute instances create ${NAME} \
     --machine-type n1-standard-2 \
     --accelerator type=${GPU_TYPE},count=${GPU_NUM} \
-    --boot-disk-size 20 \
-    --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud \
+    --image-family ubuntu-1804-lts --image-project ubuntu-os-cloud \
     --maintenance-policy TERMINATE --restart-on-failure \
     --metadata startup-script='#!/bin/bash 
         # Check for CUDA and try to install.
@@ -37,18 +36,22 @@ gcloud compute instances create ${NAME} \
           # Install cmake
           apt-get install cmake -y
 
-          curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-          dpkg -i ./cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-          apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+          # kernel headers and development packages for the currently running kernel
+          apt-get install linux-headers-$(uname -r)
+
+          curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
+          dpkg -i ./cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
+          apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
           apt-get update
-          apt-get install cuda-10-0 -y
+          apt-get install cuda -y
           rm -f ./cuda-repo*.deb
           echo >> /etc/profile
           echo "# CUDA" >> /etc/profile
-          echo "export PATH=/usr/local/cuda/bin:\$PATH" >> /etc/profile
-          echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:\$LD_LIBRARY_PATH" >> /etc/profile
+          echo "export PATH=/usr/local/cuda-10.2/bin:/usr/local/cuda-10.2/NsightCompute-2019.1${PATH:+:${PATH}}" >> /etc/profile
+          echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+" >> /etc/profile
 
-          apt-get install nvidia-cuda-toolkit -y
+          apt-get install cuda-toolkit-10-2 -y
 
           # Setup auto shutdown
           wget -O /bin/auto_shutdown.sh https://raw.githubusercontent.com/stanford-cme213/stanford-cme213.github.io/master/gcp/auto_shutdown.sh
